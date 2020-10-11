6c39ac5da7c6cb58418e15415cb2c049a845e16f
Francisco Carvajal <fraco_oxza@outlook.com>
2020-10-11 02:10:43 -0300

    tv --> tav

--- products.py
+++ products.py
@@ -1,5 +1,5 @@
 productos = [
     {"name": "laptop", "price": 800, "items": 4},
     {"name": "mouse", "price": 40, "items": 10},
-    {"name": "tv", "price": 200, "items": 2}
+    {"name": "tav", "price": 200, "items": 2}
 ]
